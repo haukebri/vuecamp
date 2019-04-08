1ad1c04bf4bba7301b59bd4dc188a1d8833f9919
Volker Krell <v.krell@reply.de>
2019-04-08 16:43:35 +0200

    Remove duplicate line, stop eslint from complaining

--- src/main.js
+++ src/main.js
@@ -1,13 +1,12 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-import Vue from 'vue'
-import BootstrapVue from 'bootstrap-vue'
+import BootstrapVue from "bootstrap-vue";
 
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 
-Vue.use(BootstrapVue)
+Vue.use(BootstrapVue);
 
 Vue.config.productionTip = false;
 
